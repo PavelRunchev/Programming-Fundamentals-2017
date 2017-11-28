49ec877b8e4984766b12138df370db85c1945a07
RAIDERS <Setty_ii@abv.bg>
2017-11-28 09:47:00 +0200

    Update

---
+++ Programming Fundamentals - Sep-2017/DictionariesLambdaLinq - Exercises/DragonArmy1/DragonArmy1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonArmy
+{
+    class Dragon
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+
+        public Dragon(string name, int damage, int health, int armor)
+        {
+            this.Name = name;
+            this.Damage = damage;
+            this.Health = health;
+            this.Armor = armor;
+        }
+    }
+
+    class DragonArmy
+    {
+        static void Main()
+        {
+            var dataDragons = new Dictionary<string, List<Dragon>>();
+            int numberOfDragons = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfDragons; i++)
+            {
+                AddDragon(dataDragons);            
+            }
+
+            PrintDragons(dataDragons);
+        }
+
+        private static void AddDragon(Dictionary<string, List<Dragon>> dataDragons)
+        {
+            string[] inputDragon = Console.ReadLine().Split(' ');
+            string type = inputDragon[0];
+            string name = inputDragon[1];
+            int damage = inputDragon[2] == "null" ? 45 : int.Parse(inputDragon[2]);
+            int health = inputDragon[3] == "null" ? 250 : int.Parse(inputDragon[3]);
+            int armor = inputDragon[4] == "null" ? 10 : int.Parse(inputDragon[4]);
+
+            if (!dataDragons.ContainsKey(type))
+            {
+                dataDragons.Add(type, new List<Dragon>());
+            }
+            if(!dataDragons[type].Any(n => n.Name == name))
+            {
+                Dragon dragon = new Dragon(name, damage, health, armor);
+                dataDragons[type].Add(dragon);
+            }
+            else
+            {
+                var curentDragon = dataDragons[type].FirstOrDefault(d => d.Name == name);
+                curentDragon.Damage = damage;
+                curentDragon.Health = health;
+                curentDragon.Armor = armor;
+            }
+        }
+
+        private static void PrintDragons(Dictionary<string, List<Dragon>> dataDragons)
+        {
+            foreach (var types in dataDragons)
+            {
+                string typeDragon = types.Key;
+                var listDragons = types.Value.OrderBy(n => n.Name).ToList();
+                double averageDamage = listDragons.Average(a => a.Damage);
+                double averageHealth = listDragons.Average(a => a.Health);
+                double averageArmor = listDragons.Average(a => a.Armor);
+                Console.WriteLine($"{typeDragon}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+                foreach (var dragon in listDragons)
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - Sep-2017/DictionariesLambdaLinq - Exercises/DragonArmy2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Dragon
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+
+        public Dragon(string name, int damage, int health, int armor)
+        {
+            this.Name = name;
+            this.Damage = damage;
+            this.Health = health;
+            this.Armor = armor;
+        }
+    }
+
+    class TypeDragon
+    {
+        public string Type { get; set; }
+        public List<Dragon> Dragons { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var dataDragons = new List<TypeDragon>();
+            int numberOfDragons = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfDragons; i++)
+            {
+                string[] inputDragon = Console.ReadLine().Split(' ');
+                string type = inputDragon[0];
+                string name = inputDragon[1];
+                int damage = inputDragon[2] == "null" ? 45 : int.Parse(inputDragon[2]);
+                int health = inputDragon[3] == "null" ? 250 : int.Parse(inputDragon[3]);
+                int armor = inputDragon[4] == "null" ? 10 : int.Parse(inputDragon[4]);
+
+                if ((!dataDragons.Any(a => a.Type == type)))
+                {
+                    Dragon dragon = new Dragon(name, damage, health, armor);
+                    TypeDragon typeDragon = new TypeDragon();
+                    typeDragon.Type = type;
+                    typeDragon.Dragons = new List<Dragon>();
+                    typeDragon.Dragons.Add(dragon);
+                    dataDragons.Add(typeDragon);
+                }
+                else if (dataDragons.Any(t => t.Type == type))
+                {
+                    var curentType = dataDragons.FirstOrDefault(t => t.Type == type);
+                    if (!curentType.Dragons.Any(n => n.Name == name))
+                    {
+                        Dragon dragon = new Dragon(name, damage, health, armor);
+                        curentType.Dragons.Add(dragon);
+                    }
+                    else
+                    {
+                        var curentDragon = curentType.Dragons.FirstOrDefault(n => n.Name == name);
+                        curentDragon.Damage = damage;
+                        curentDragon.Health = health;
+                        curentDragon.Armor = armor;
+                    }
+                }
+            }
+
+            PrintDragons(dataDragons);
+        }
+
+        private static void PrintDragons(List<TypeDragon> dataDragons)
+        {
+            foreach (var types in dataDragons)
+            {
+                string typeDragon = types.Type;
+                var listDragons = types.Dragons.OrderBy(n => n.Name).ToList();
+                double averageDamage = listDragons.Average(a => a.Damage);
+                double averageHealth = listDragons.Average(a => a.Health);
+                double averageArmor = listDragons.Average(a => a.Armor);
+                Console.WriteLine($"{typeDragon}::({averageDamage:f2}/{averageHealth:f2}/{averageArmor:f2})");
+                foreach (var dragon in listDragons)
+                {
+                    Console.WriteLine($"-{dragon.Name} -> damage: {dragon.Damage}, health: {dragon.Health}, armor: {dragon.Armor}");
+                }
+            }
+        }
+    }
+}
